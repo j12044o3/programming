bc81225fc8a36050d368d6186476bd1cc039c49b
j12044o3 <62865781+j12044o3@users.noreply.github.com>
2020-05-20 15:53:07 +0900

    Add files via upload

---
+++ 0520/arith.py
@@ -0,0 +1,11 @@
+# 파이썬 산술 연산자 종류
+a = int(input("첫 번째 수 입력: "))
+b = int(input("두 번째 수 입력: "))
+
+print(a, '+', b, '=', (a+b))
+print(a, '+', b, '=', (a-b))
+print(a, '+', b, '=', (a*b))
+print(a, '+', b, '=', (a/b))
+print(a, '+', b, '=', (a//b))
+print(a, '+', b, '=', (a&b))
+print(a, '+', b, '=', (a**b))
